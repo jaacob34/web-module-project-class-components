e8ded6722f696dbdd3b272610896c3f479be605d
jaacob34 <jacobhernandez34@icloud.com>
2025-05-13 15:13:39 -0700

    added state to app comp and function to form comp to push new todo in todo state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import Form from './Form'
 
 export default class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1232423,
+          completed: false
+        },
+      ],
+      input: '',
+      disabled: true
+    }
+  }
+
+  handleChange = (evt) => {
+    this.setState({input: evt.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    this.setState({todos: [...this.state.todos, {
+      name: this.state.input,
+      id: Date.now(),
+      completed: false
+
+    }]})
+    
+    this.setState({input: ''})
+  }
+
   render() {
     return (
       <div>
         Todo App
+        <Form handleChange={this.handleChange} value={this.state.input} submit={this.handleSubmit} />
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -3,9 +3,11 @@ import React from 'react'
 export default class Form extends React.Component {
   render() {
     return (
-      <div>
-        Form
-      </div>
+      <form onSubmit={this.props.submit}>
+        <input type='text' name='todo' value={this.props.value} onChange={this.props.handleChange}/>
+        <button type='submit' disabled={!this.props.value.trim()}>Submit</button>
+      </form>
     )
   }
 }
+
